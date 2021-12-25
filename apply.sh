ccd941c4d64835091d2a110c780b14ce2afa3371
RiedleroD <riedler@gelse.eu>
2021-12-25 20:52:19 +0100

    added helper scripts

---
+++ apply.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+cp kitty.conf ~/.config/kitty/kitty.conf
+cp makepkg.conf /etc/makepkg.conf
+cp qtile.config.py ~/.config/qtile/config.py
+cp sway.conf ~/.config/sway/config
+cp waybar.conf ~/.config/waybar/config
+cp waybar.css ~/.config/waybar/style.css
+
+curl https://i.imgur.com/2kyvrtg.png > /home/riedler/Pictures/wallpaper.png
+curl https://i.imgur.com/e2rqx1O.jpg > /home/riedler/Pictures/wallpaper.jpg
+
+echo "Make sure you installed the dependencies!"
---
+++ dependencies.sh
@@ -0,0 +1,1 @@
+pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font libcanberra otf-font-awesome
