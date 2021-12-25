44b6a454691d99eab8826fea791517386cee7509
RiedleroD <riedler@gelse.eu>
2021-12-25 22:11:15 +0100

    added qt5 styling

--- apply.sh
+++ apply.sh
@@ -3,12 +3,16 @@ mkdir ~/.bin_replacements
 cp .bash_profile ~/.bash_profile
 cp .bashrc ~/.bashrc
 
-mkdir ~/.config/kitty ~/.config/qtile ~/.config/sway ~/.config/waybar -p
+mkdir ~/.config/kitty ~/.config/qtile ~/.config/sway ~/.config/waybar ~/.config/qt5ct/qss ~/.config/Kvantum/ArcDark# -p
 cp kitty.conf ~/.config/kitty/kitty.conf
 cp qtile.config.py ~/.config/qtile/config.py
 cp sway.conf ~/.config/sway/config
 cp waybar.conf ~/.config/waybar/config
 cp waybar.css ~/.config/waybar/style.css
+cp qt5ct.conf ~/.config/qt5ct/qt5ct.conf
+cp qt5ct_spinboxes.qss ~/.config/qt5ct/qss/spinboxes.qss
+cp kvantum.kvconfig ~/.config/Kvantum/kvantum.kvconfig
+cp ArcDarc.kvconfig ~/.config/Kvantum/ArcDark#/ArcDark#.kvconfig
 doas cp makepkg.conf /etc/makepkg.conf
 
 mkdir /home/riedler/Pictures -p
--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,4 @@
 paru -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
 libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex pcmanfm \
-brightnessctl nwg-menu deadbeef-git exa --needed
+brightnessctl nwg-menu deadbeef-git exa qt5ct kvantum-qt5 kvantum-theme-arc \
+papirus-icon-theme --needed
