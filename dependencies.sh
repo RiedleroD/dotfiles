35bd03248c24c77872b6fb54bfcd0f3e2c7729e8
RiedleroD <riedler@gelse.eu>
2021-12-25 21:12:26 +0100

    added more dependencies for the sway config

--- dependencies.sh
+++ dependencies.sh
@@ -1,1 +1,3 @@
-pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font libcanberra otf-font-awesome opendoas
+pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
+libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex nwg-menu \
+pcmanfm brightnessctl deadbeef
