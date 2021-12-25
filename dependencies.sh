a00d24115a902964b145fc2672a3a468c98475af
RiedleroD <riedler@gelse.eu>
2021-12-25 21:38:58 +0100

    added flag to dependencies to tell paru to not reinstall existing packages

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 paru -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
 libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex pcmanfm \
-brightnessctl nwg-menu deadbeef-git
+brightnessctl nwg-menu deadbeef-git --needed
