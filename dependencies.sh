fe4bd8b3a19d86a9d9b6646845dfea3342360307
RiedleroD <riedler@gelse.eu>
2021-12-25 22:30:34 +0100

    added pipewire to deps

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,7 @@
 paru -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
 libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex pcmanfm \
 brightnessctl nwg-menu deadbeef-git exa qt5ct kvantum-qt5 kvantum-theme-arc \
-papirus-icon-theme --needed
+papirus-icon-theme pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa \
+pipewire-v4l2 pipewire-media-session helvum gst-plugin-pipewire --needed
+
+echo "reboot after this."
