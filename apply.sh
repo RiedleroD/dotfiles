50641236cd1725b6b9051e98d0f01f0b1d342c51
RiedleroD <riedler@gelse.eu>
2021-12-25 21:00:10 +0100

    fixed some oversights

--- apply.sh
+++ apply.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/sh
+mkdir ~/.config/kitty ~/.config/qtile ~/.config/sway ~/.config/waybar -p
 cp kitty.conf ~/.config/kitty/kitty.conf
-cp makepkg.conf /etc/makepkg.conf
 cp qtile.config.py ~/.config/qtile/config.py
 cp sway.conf ~/.config/sway/config
 cp waybar.conf ~/.config/waybar/config
 cp waybar.css ~/.config/waybar/style.css
+doas cp makepkg.conf /etc/makepkg.conf
 
 curl https://i.imgur.com/2kyvrtg.png > /home/riedler/Pictures/wallpaper.png
 curl https://i.imgur.com/e2rqx1O.jpg > /home/riedler/Pictures/wallpaper.jpg
--- dependencies.sh
+++ dependencies.sh
@@ -1,1 +1,1 @@
-pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font libcanberra otf-font-awesome
+pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font libcanberra otf-font-awesome opendoas
