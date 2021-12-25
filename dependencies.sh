14aa7532f28596bf8f291b863fbe09960b1f6ad5
RiedleroD <riedler@gelse.eu>
2021-12-25 21:16:11 +0100

    whoops some of those were AUR packages

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
-pacman -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
-libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex nwg-menu \
-pcmanfm brightnessctl deadbeef
+paru -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
+libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex pcmanfm \
+brightnessctl nwg-menu deadbeef-git
