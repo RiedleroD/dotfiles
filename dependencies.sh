a9f354e1f6ed0ec908860fef102dda2fd9c9365e
RiedleroD <riedler@gelse.eu>
2021-12-26 10:38:33 +0100

    added xwayland as dependency

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,8 @@
-paru -S xdg-user-dirs sway waybar waylock qtile kitty libertinus-font \
-libcanberra otf-font-awesome opendoas bemenu bemenu-wayland dex pcmanfm \
-brightnessctl nwg-menu deadbeef-git exa qt5ct kvantum-qt5 kvantum-theme-arc \
-papirus-icon-theme pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa \
-pipewire-v4l2 pipewire-media-session helvum gst-plugin-pipewire --needed
+paru -S xdg-user-dirs sway waybar waylock xorg-xwayland qtile kitty \
+libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
+dex pcmanfm brightnessctl nwg-menu deadbeef-git exa qt5ct kvantum-qt5 \
+kvantum-theme-arc papirus-icon-theme pipewire pipewire-pulse \
+pipewire-jack-dropin pipewire-alsa pipewire-v4l2 pipewire-media-session \
+helvum gst-plugin-pipewire --needed
 
 echo "reboot after this."
