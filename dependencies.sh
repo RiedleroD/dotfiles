92cf8cbc07a8d0fbc2ad5dec181839154cc752c0
RiedleroD <riedler@gelse.eu>
2021-12-26 14:39:39 +0100

    added some dependencies needed for music

--- dependencies.sh
+++ dependencies.sh
@@ -3,7 +3,10 @@ libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
 dex pcmanfm brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
 kvantum-qt5 kvantum-theme-arc arc-gtk-theme papirus-icon-theme \
 deepin-icon-theme pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa \
-pipewire-v4l2 pipewire-media-session helvum gst-plugin-pipewire \
+pipewire-v4l2 pipewire-media-session helvum gst-plugin-pipewire youtube-dl \
 flameshot ttf-joypixels --needed
+
+python -m ensurepip
+pip3 install mutagen
 
 echo "reboot after this."
