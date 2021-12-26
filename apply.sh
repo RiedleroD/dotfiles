764d014bc1dfa01eb58956463b4e6dfa9514058a
RiedleroD <riedler@gelse.eu>
2021-12-26 10:49:22 +0100

    added gtk configs

--- apply.sh
+++ apply.sh
@@ -3,7 +3,9 @@ mkdir ~/.bin_replacements
 cp .bash_profile ~/.bash_profile
 cp .bashrc ~/.bashrc
 
-mkdir ~/.config/kitty ~/.config/qtile ~/.config/sway ~/.config/waybar ~/.config/qt5ct/qss ~/.config/Kvantum/ArcDark# -p
+mkdir ~/.config/kitty ~/.config/qtile ~/.config/sway ~/.config/waybar \
+~/.config/qt5ct/qss ~/.config/Kvantum/ArcDark# ~/.config/gtk-2.0 \
+~/.config/gtk-3.0 -p
 cp kitty.conf ~/.config/kitty/kitty.conf
 cp qtile.config.py ~/.config/qtile/config.py
 cp sway.conf ~/.config/sway/config
@@ -13,6 +15,9 @@ cp qt5ct.conf ~/.config/qt5ct/qt5ct.conf
 cp qt5ct_spinboxes.qss ~/.config/qt5ct/qss/spinboxes.qss
 cp kvantum.kvconfig ~/.config/Kvantum/kvantum.kvconfig
 cp ArcDark.kvconfig ~/.config/Kvantum/ArcDark#/ArcDark#.kvconfig
+cp .gtkrc-2.0 ~/.gtkrc-2.0
+cp gtk2-filechooser.ini ~/.config/gtk-2.0/gtkfilechooser.ini
+cp gtk3.ini ~/.config/gtk-3.0/settings.ini
 doas cp makepkg.conf /etc/makepkg.conf
 
 mkdir /home/riedler/Pictures -p
