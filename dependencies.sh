2e90ce81b0af81b87e9d36ada3bf3dfe4910c069
RiedleroD <riedler@gelse.eu>
2021-12-26 10:53:27 +0100

    added gtk3 theme to deps

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,8 @@
 paru -S xdg-user-dirs sway waybar waylock xorg-xwayland qtile kitty \
 libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
 dex pcmanfm brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
-kvantum-qt5 kvantum-theme-arc papirus-icon-theme pipewire pipewire-pulse \
-pipewire-jack-dropin pipewire-alsa pipewire-v4l2 pipewire-media-session \
-helvum gst-plugin-pipewire --needed
+kvantum-qt5 kvantum-theme-arc arc-gtk-theme papirus-icon-theme pipewire \
+pipewire-pulse pipewire-jack-dropin pipewire-alsa pipewire-v4l2 \
+pipewire-media-session helvum gst-plugin-pipewire --needed
 
 echo "reboot after this."
