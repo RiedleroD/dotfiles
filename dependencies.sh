f1410251419a80b1d410d60cf4f227aeec25ac35
RiedleroD <riedler@gelse.eu>
2021-12-27 13:34:44 +0100

    fixed dependencies as somehow kvantum-qt5 *just* got renamed to kvantum

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,10 @@
 paru -S xdg-user-dirs sway waybar waylock xorg-xwayland qtile kitty \
 libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
 dex pcmanfm brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
-kvantum-qt5 kvantum-theme-arc arc-gtk-theme papirus-icon-theme \
-deepin-icon-theme pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa \
-pipewire-v4l2 pipewire-media-session helvum gst-plugin-pipewire youtube-dl \
-flameshot ttf-joypixels opusfile engrampa p7zip unrar discord_arch_electron \
+kvantum kvantum-theme-arc arc-gtk-theme papirus-icon-theme deepin-icon-theme \
+pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa pipewire-v4l2 \
+pipewire-media-session helvum gst-plugin-pipewire youtube-dl flameshot \
+ttf-joypixels opusfile engrampa p7zip unrar discord_arch_electron \
 steam-native-runtime teams-for-linux gimp libreoffice-fresh gthumb inkscape \
 google-chrome chromium firefox icaclient geogebra gummi wxmaxima typora-free \
 audacity carla obs-studio wlrobs lmms pavucontrol timidity++ vlc alacritty \
