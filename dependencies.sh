e4f366147d13a52f2e1270be671c61ff758cd5a9
RiedleroD <riedler@gelse.eu>
2021-12-31 00:10:43 +0100

    deps: changed pcmanfm to its gtk3 variant

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 paru -S xdg-user-dirs sway waybar waylock xorg-xwayland qtile kitty \
 libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
-dex pcmanfm brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
+dex pcmanfm-gtk3 brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
 kvantum kvantum-theme-arc arc-gtk-theme papirus-icon-theme deepin-icon-theme \
 pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa pipewire-v4l2 \
 pipewire-media-session helvum gst-plugin-pipewire youtube-dl flameshot \
