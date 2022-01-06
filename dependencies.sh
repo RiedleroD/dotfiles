9ded282f96a32a0cfdedc2dda207ec1d1a884068
RiedleroD <riedler@gelse.eu>
2022-01-06 16:01:58 +0100

    wow am I dumb

--- dependencies.sh
+++ dependencies.sh
@@ -3,15 +3,15 @@ libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
 dex pcmanfm-gtk3 brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
 kvantum kvantum-theme-arc arc-gtk-theme papirus-icon-theme deepin-icon-theme \
 pipewire pipewire-pulse pipewire-jack-dropin pipewire-alsa pipewire-v4l2 \
-pipewire-media-session helvum gst-plugin-pipewire youtube-dl flameshot \
-ttf-joypixels opusfile engrampa p7zip unrar discord_arch_electron \
-steam-native-runtime teams-for-linux gimp libreoffice-fresh gthumb inkscape \
-google-chrome chromium firefox icaclient geogebra gummi wxmaxima typora-free \
-audacity carla obs-studio wlrobs lmms pavucontrol timidity++ vlc alacritty \
-htop base-devel git cmake qt5-base qt5-tools qt5-x11extras libsndfile fftw \
-libvorbis lame libsamplerate libogg wine stk fluidsynth fltk libgig jack2 sdl \
-alsa-lib portaudio perl-list-moreutils perl-exporter-tiny perl-xml-parser \
-sfarkxtc mako wireplumber grim imv blueman --needed
+wireplumber helvum gst-plugin-pipewire youtube-dl flameshot ttf-joypixels \
+opusfile engrampa p7zip unrar discord_arch_electron steam-native-runtime \
+teams-for-linux gimp libreoffice-fresh gthumb inkscape google-chrome chromium \
+firefox icaclient geogebra gummi wxmaxima typora-free audacity carla \
+obs-studio wlrobs lmms pavucontrol timidity++ vlc alacritty htop base-devel \
+git cmake qt5-base qt5-tools qt5-x11extras libsndfile fftw libvorbis lame \
+libsamplerate libogg wine stk fluidsynth fltk libgig jack2 sdl alsa-lib \
+portaudio perl-list-moreutils perl-exporter-tiny perl-xml-parser sfarkxtc mako \
+grim imv blueman --needed
 
 systemctl --user enable wireplumber
 sudo systemctl enable --now bluetooth
