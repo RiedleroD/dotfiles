107480dcf06b9f1e427d93926e75261c7d94ccc9
RiedleroD <riedler@gelse.eu>
2022-03-05 12:50:38 +0100

    removed grim in favour of flameshot

--- dependencies.sh
+++ dependencies.sh
@@ -11,7 +11,7 @@ obs-studio wlrobs lmms pavucontrol timidity++ vlc htop base-devel \
 git cmake qt5-base qt5-tools qt5-x11extras libsndfile fftw libvorbis lame \
 libsamplerate libogg wine stk fluidsynth fltk libgig jack2 sdl alsa-lib \
 portaudio perl-list-moreutils perl-exporter-tiny perl-xml-parser sfarkxtc mako \
-grim imv blueman gnome-themes-extra gtk-engine-murrine gvfs gvfs-smb \
+imv blueman gnome-themes-extra gtk-engine-murrine gvfs gvfs-smb \
 gvfs-mtp gvfs-nfs --needed
 
 systemctl --user enable wireplumber
