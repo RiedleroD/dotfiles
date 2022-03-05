362c8ea774d80efe854bfe80e63807db81e61ff8
RiedleroD <riedler@gelse.eu>
2022-03-05 12:44:00 +0100

    removed qtile/X11 from setup

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
-paru -S xdg-user-dirs sway waybar waylock xorg-xwayland qtile kitty \
+paru -S xdg-user-dirs sway waybar waylock xorg-xwayland kitty \
 libertinus-font libcanberra otf-font-awesome opendoas bemenu bemenu-wayland \
 dex pcmanfm-gtk3 brightnessctl nwg-menu deadbeef-git exa lxappearance qt5ct \
 kvantum kvantum-theme-arc arc-gtk-theme papirus-icon-theme deepin-icon-theme \
@@ -7,7 +7,7 @@ wireplumber helvum gst-plugin-pipewire youtube-dl flameshot ttf-joypixels \
 opusfile engrampa p7zip unrar discord_arch_electron steam-native-runtime \
 teams-for-linux gimp libreoffice-fresh gthumb inkscape google-chrome chromium \
 firefox icaclient geogebra gummi wxmaxima typora-free audacity carla \
-obs-studio wlrobs lmms pavucontrol timidity++ vlc alacritty htop base-devel \
+obs-studio wlrobs lmms pavucontrol timidity++ vlc htop base-devel \
 git cmake qt5-base qt5-tools qt5-x11extras libsndfile fftw libvorbis lame \
 libsamplerate libogg wine stk fluidsynth fltk libgig jack2 sdl alsa-lib \
 portaudio perl-list-moreutils perl-exporter-tiny perl-xml-parser sfarkxtc mako \
