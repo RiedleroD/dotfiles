d5e40034397117eeb7e47590c53f99898a1afb71
RiedleroD <github@riedler.wien>
2023-04-14 19:14:45 +0200

    screenshotworks without manual grim now

--- .bin_replacements/lock.sh
+++ .bin_replacements/lock.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
-grim /tmp/lockimg.png && \
-swaylock -i /tmp/lockimg.png --effect-blur "10x20" --fade-in 1 --grace 1
+#grim -t png /tmp/lockimg.png && \
+#swaylock -i /tmp/lockimg.png --effect-blur "10x20" --fade-in 1 --grace 1
+swaylock -S --effect-blur "10x20" --fade-in 1 --grace 1
