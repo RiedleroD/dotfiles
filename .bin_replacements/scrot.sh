b483f33cd9e5eafb74c0ae3e2908a5e682fd242c
RiedleroD <github@riedler.wien>
2024-02-18 22:23:36 +0100

    added better screenshot tool

---
+++ .bin_replacements/scrot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# pretty simple screenshot thing, but still handy
+
+filename="$(date +%s).png"
+filepath="$HOME/Pictures/screenshots/$filename"
+
+grim -g "$(slurp)" -l 9 -c "$filepath"
+wl-copy < "$filepath"
